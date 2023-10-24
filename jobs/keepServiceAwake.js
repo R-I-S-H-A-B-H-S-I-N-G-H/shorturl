485664ecd9318241898bbab770245014a54e83ee
rishabh <rishabhsingh2305@gmail.com>
2023-10-24 23:59:54 +0530

    increased the ping durration

--- jobs/keepServiceAwake.js
+++ jobs/keepServiceAwake.js
@@ -1,7 +1,7 @@
 const schedule = require("node-schedule");
 const axios = require("axios");
 exports.ping = () =>
-	schedule.scheduleJob("*/10 * * * * *", async () => {
+	schedule.scheduleJob("* */15 * * * *", async () => {
 		const res = await axios.get(process.env.PING_ENDPOINT);
 		console.log("PINGING THE API TO KEEP ALIVE!!", res.status);
 	});
