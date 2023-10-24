2a59905d00c9981c6d60f677065bec4ecefd72c3
rishabh <rishabhsingh2305@gmail.com>
2023-10-24 22:20:54 +0530

    added code refactoring

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -1,5 +1,46 @@
 const URL = require("../models/urlModel");
+const { generateShortId } = require("../utils/shortIdGenerator");
+
+exports.aboutUrlApi = (req, res, next) => {
+	res.json({
+		Title: "This is a Url shortner APi",
+		EndPoints: [
+			{
+				method: "POST",
+				path: "",
+				action: "create short url",
+			},
+			{
+				method: "GET",
+				path: "/:shortId",
+				action: "redirect to the original url",
+			},
+		],
+	});
+};
+
+exports.getByShortId = async (req, res, next) => {
+	try {
+		const query = URL.where({ shortId: req.params.shortId });
+		const urlResp = await query.findOne();
+		const redirectURL = urlResp.redirectURL;
+		res.redirect(redirectURL);
+	} catch (error) {
+		console.error("SHORT ID DOESNT EXIST", error);
+		return res.status(500).json({ error: "SHORT ID DOESNT EXIST" });
+	}
+};
+
 exports.createUrl = async (req, res, next) => {
 	try {
-	} catch (error) {}
+		url = await URL.create({
+			shortId: generateShortId(),
+			redirectURL: req.body.redirectURL,
+			visitHistory: [],
+		});
+		res.json({ url });
+	} catch (error) {
+		console.error("Failed to generate short URL:", error);
+		return res.status(500).json({ error: "Failed to generate short URL" });
+	}
 };
--- routes/urlRoutes.js
+++ routes/urlRoutes.js
@@ -1,33 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const urlController = require("../controllers/urlController");
-const { generateShortId } = require("../utils/shortIdGenerator");
-const URL = require("../models/urlModel");
+const {
+	createUrl,
+	getByShortId,
+	aboutUrlApi,
+} = require("../controllers/urlController");
 
-router.post("", async (req, res, next) => {
-	try {
-		url = await URL.create({
-			shortId: generateShortId(),
-			redirectURL: req.body.redirectURL,
-			visitHistory: [],
-		});
-		res.json({ url });
-	} catch (error) {
-		console.error("Failed to generate short URL:", error);
-		return res.status(500).json({ error: "Failed to generate short URL" });
-	}
-});
-
-router.get("/:shortId", async (req, res, next) => {
-	try {
-		const query = URL.where({ shortId: req.params.shortId });
-		const urlResp = await query.findOne();
-		const redirectURL = urlResp.redirectURL;
-		res.redirect(redirectURL);
-	} catch (error) {
-		console.error("SHORT ID DOESNT EXIST", error);
-		return res.status(500).json({ error: "SHORT ID DOESNT EXIST" });
-	}
-});
+router.get("/:shortId", getByShortId);
+router.post("", createUrl).get("", aboutUrlApi);
 
 module.exports = router;
