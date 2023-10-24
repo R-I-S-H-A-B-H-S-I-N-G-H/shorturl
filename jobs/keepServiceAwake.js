0dba2b33b4152eff30c3df1ec56145e772d0ee40
rishabh <rishabhsingh2305@gmail.com>
2023-10-24 22:49:10 +0530

    added pinging to keep service alive

--- jobs/keepServiceAwake.js
+++ jobs/keepServiceAwake.js
@@ -1,6 +1,7 @@
 const schedule = require("node-schedule");
+const axios = require("axios");
 exports.ping = () =>
 	schedule.scheduleJob("*/10 * * * * *", async () => {
-		const res = await fetch(`http://localhost:${process.env.PORT}`);
+		const res = await axios.get(`http://localhost:${process.env.PORT}`);
 		console.log("PINGING THE API TO KEEP ALIVE!!", res.status);
 	});
