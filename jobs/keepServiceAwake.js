55cbc4f37785d23907065f80271e53fbe96ae0a9
rishabh <rishabhsingh2305@gmail.com>
2023-10-24 23:51:33 +0530

    changes ping end point

--- jobs/keepServiceAwake.js
+++ jobs/keepServiceAwake.js
@@ -2,6 +2,6 @@ const schedule = require("node-schedule");
 const axios = require("axios");
 exports.ping = () =>
 	schedule.scheduleJob("*/10 * * * * *", async () => {
-		const res = await axios.get(`http://localhost:${process.env.PORT}`);
+		const res = await axios.get(process.env.PING_ENDPOINT);
 		console.log("PINGING THE API TO KEEP ALIVE!!", res.status);
 	});
